8511d70b4d3b2efee174ed38c9862c6d951ea6d9
fangbingluo <fl358@cornell.edu>
2015-12-09 12:10:47 -0500

    Delete rbtree.js

--- rbtree.js
+++
@@ -1,8 +0,0 @@
-var helper = require('./helper1.js');
-var _ = require('lodash');
-
-
-var helper.key = 1;
-var helper.color = "black";
-var helper.test = new helper.parent(key, color);
-helper.test.start(); 
